29d61f03fc644a06ef1532401e0bdf38887e48d4
juliancpy <juliancacrs@gmail.com>
2023-05-26 16:58:25 -0400

    Adding menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parentes New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kake, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
